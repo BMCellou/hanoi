4539a9bb550bf9d3ca2a5673c91a4de514718c33
Cellou <celpsy4@hotmail.fr>
2018-01-28 23:24:08 +0100

    Update TourTest.java

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -7,8 +7,19 @@ import org.junit.Test;
 public class TourTest {
 
 	@Test
-	public void test() {
-
+	public void testEmpiler() {
+		
+		Tour T = new Tour();
+		
+		Disque pe = new Disque(1);
+		Disque petit = new Disque(2);
+		Disque moyen = new Disque(3);
+		Disque grand = new Disque(4);
+		assertTrue("peut pas empiler",T.empiler(petit));
+		assertTrue("peut empiler",T.empiler(grand) );
+		assertTrue("peut  empiler",T.empiler(moyen) );
+		assertTrue("peut empiler",T.empiler(petit));
+		
+		
 	}
-
 }
